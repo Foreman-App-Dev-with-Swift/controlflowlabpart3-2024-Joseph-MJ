a133e7a609dac2daf6c0efd9f81f06e2b108fcc5
Joseph Mendieta J <jmendietajara@cps.edu>
2024-11-20 11:42:51 -0600

    11/20/2024

--- ControlFlowLabPart3/main.swift
+++ ControlFlowLabPart3/main.swift
@@ -157,11 +157,10 @@ print("***PROBLEM 3.3 : COMBINING ALL LOGIC")
 let isRaining = true
 let isSunny = true
 var temp = 90
-let isNiceWeather = 82
-if (isNiceWeather >= 82) {
+let isNiceWeather = (!isRaining && isSunny && temp > 82)
+if (isNiceWeather) {
     print("I'm going for a walk!")
 }
-    
 print("\n")
 print("***PROBLEM 3.4 : FINISHING THE FITNESS APP")
 /*
@@ -174,5 +173,18 @@ print("***PROBLEM 3.4 : FINISHING THE FITNESS APP")
 
 
 //Once you complete the coding, test and run the code with various values for currentHR to see if all the coding works. If all the coding works, check your work and see if it shows in the console. Then you are done! Push it up to GitHub.
-
+var currentHR = 90
+var targetHR = 100
+let isInTarget = currentHR == targetHR
+let isBelowTarget = currentHR < targetHR
+let isAboveTarget = currentHR > targetHR
+if (isInTarget){
+    print("You're right on track!")
+}
+else if (isBelowTarget) {
+    print("You're doing great, but try to push it a bit!")
+}
+if (isAboveTarget) {
+    print("You're on fire! Slow it down just a bit.")
+}
 print("\n")
